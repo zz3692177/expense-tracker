b7c0d867385464d24a175b78274b2ba9b7d3b081
wuky <zz3692177@yahoo.com.tw>
2020-11-21 01:16:26 +0800

    feat: add expensive seeder

---
+++ models/seeds/expensiveSeeder.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+const Expensive = require('../expensive') // 載入 todo model
+mongoose.connect('mongodb://localhost/expensive', { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+  console.log(new Date(2010, 10, 10))
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+  for (let i = 0; i < 10; i++) {
+    Expensive.create({ title: 'title-' + i, date: new Date(), category: 'traffic', amount: 20 })
+  }
+  console.log('done')
+})
