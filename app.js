b98bdcee63ad1715355c301afa7171a2265a12e9
wuky <zz3692177@yahoo.com.tw>
2020-11-25 02:15:30 +0800

    fix: edit req.body name & delete route feat: body-parser

--- app.js
+++ app.js
@@ -5,51 +5,84 @@ const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/expensive', { useNewUrlParser: true, useUnifiedTopology: true })
 
 const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser')
+
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main', extname: '.handlebars' }))
 app.set('view engine', 'handlebars')
 
 app.use(express.static('public'))
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
 const Record = require('./models/record');
-// const icon = [{ "家居物業": "<i class=\"fas fa - home\"></i>" }, {
-//   "交通出行": "<i class=\"fas fa- shuttle - van\">"
-// }, { "休閒娛樂": "<i class=\"fas fa- grin - beam\">" }, { "餐飲食品": "<i class=\"fas fa- utensils\">" }, { "其他": "<i class=\"fas fa- pen\">" }]
+
 
 const icon = {
-  "家居物業": "<i class=\"fas fa - home\"></i>", "交通出行": "<i class=\"fas fa- shuttle - van\">", "休閒娛樂": "<i class=\"fas fa- grin - beam\">",
-  "餐飲食品": "<i class=\"fas fa- utensils\">", "其他": "<i class=\"fas fa- pen\">"
+  "家居物業": "<i class=\"fas fa-home\"></i>", "交通出行": "<i class=\"fas fa-shuttle-van\"></i>", "休閒娛樂": "<i class=\"fas fa-grin-beam\"></i>",
+  "餐飲食品": "<i class=\"fas fa-utensils\"></i>", "其他": "<i class=\"fas fa-pen\"></i>"
 }
 
-app.post('/record/:id', (req, res) => {
-  const id = req.params.id
-  return Record.findById(id)
-    .lean()
-    .then(Record => { res.render('edit', { record }) })
-    .catch(error => console.log(error))
-})
+
 
 app.get('/', (req, res) => {
-
+  console.log(Record.find())
   Record.find()
     .lean()
     .then(records => {
       res.render('index', { records, icon })
-      console.log(icon)
+      // console.log(icon)
     })
 
+})
+
+app.get('/record/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Record.findById(id)
+    .lean()
+    .then(record => {
+      res.render('edit', { record })
+      // console.log(record)
+    })
+    .catch(error => console.log(error))
+})
+
+app.post('/record/:id', (req, res) => {
+  const id = req.params.id
+  return Record.findById(id)
+    .then(record => {
+      record = Object.assign(record, req.body)
+      record.icon = icon[record.category]
+      console.log(record.icon)
+      return record.save()
+    })
+    .then(() => {
+      res.redirect(`/`)
+    })
+    .catch(error => console.log(error))
+})
+
+app.post('/record', (req, res) => {
+  console.log(req.body)
+  const newOne = Object.assign(req.body)
+  newOne.icon = icon[newOne.category]
+  return Record.create(newOne)
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
 })
 
 app.get('/record/new', (req, res) => {
   res.render('new')
 })
-// app.post('/record', (req, res) => {
-//   const newOne = Object.assign(req.body)      // 從 req.body 拿出表單裡的 name 資料
-//   return Restaurant.create(newOne)     // 存入資料庫
-//     .then(() => res.redirect('/')) // 新增完成後導回首頁
-//     .catch(error => console.log(error))
 
-// })
+app.post('/record/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Record.findById(id)
+    .then(record => record.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
 // 取得資料庫連線狀態
 const db = mongoose.connection
 // 連線異常
